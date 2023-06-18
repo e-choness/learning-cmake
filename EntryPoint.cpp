bd6f962c94e34d178db49e80a3ea5019a59a90d9
Echo Yin <echo_ybl@hotmail.com>
2023-06-18 16:48:28 -0400

    trying liskov substitution, create interface for each function

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -2,12 +2,14 @@
 #include "src/TypeErasure/Cow.h"
 #include "src/TypeErasure/Dog.h"
 #include "src/SolidPrinciples/BetterFilter.h"
-
+#include "src/LiskovSubstitution/Rectangle.h"
+#include "src/LiskovSubstitution/MechineFunctions.h"
 void TestTypeErasure();
 void TestSingleResponsibility();
+void TestLiskov();
 
 int main(int argc, char **argv){
-    TestSingleResponsibility();
+    TestLiskov();
 
     return 0;
 }
@@ -33,4 +35,24 @@ void TestSingleResponsibility(){
     auto blueSmallThings = betterFilter.filter(all, blueAndSmall);
     for(auto& x: blueSmallThings)
         std::cout<<x->name<<" is blue and small." <<std::endl;
+}
+
+void TestLiskov(){
+    auto rect = std::vector<Rectangle>{
+        Rectangle(5,6),
+        Rectangle(2,9)
+    };
+
+    for(auto& r:rect){
+        std::cout << r.area() << std::endl;
+    }
+
+    auto documents = std::vector<Document*>();
+    documents.push_back(new Document("file 1", "This is file 1 m_Content."));
+    documents.push_back(new Document("file 2", "This is file 2 m_Content."));
+    class Machine: public IMachine{};
+
+    auto machine = Machine();
+    machine.print(documents);
+    machine.scan(documents);
 }
