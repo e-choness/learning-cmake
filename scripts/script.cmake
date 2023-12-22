7956a917b9a724138e7c13ff2ff0b361ec201b2c
Echo Yin <echo_ybl@hotmail.com>
2023-12-22 11:12:58 -0500

    Revamp repository

--- scripts/script.cmake
+++
@@ -1,3 +0,0 @@
-cmake_minimum_required(VERSION 3.20.0)
-message("Writing a file from a c-make script")
-file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/docs/Hello.txt "This file is written by a c-make script.")
--- src/OperatorOverride.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Echo Yin on 2023-07-14.
-//
-#include "OperatorOverride.hpp"
-
-Object::Object(int a, int b):m_A(a),m_B(b) {
-
-}
